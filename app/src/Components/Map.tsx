81b8e614330eae5bca86b9bb78dbe15582dc429c
NIRANKEN <nirank.8192cpjpyjs@gmail.com>
2023-02-04 22:05:58 +0900

    make gmap-embed-api-key secret

--- app/src/Components/Map.tsx
+++ app/src/Components/Map.tsx
@@ -7,11 +7,12 @@ type MapProps = {
 export const Map: React.FC<MapProps> = ({ placeName }) => {
   // 出てくる図がかなり拡大されているので、もう２段階くらい縮小したものがよい
   // 地図自体のサイズ調整をする。
+  const gmapApiKey: string = process.env.REACT_APP_GMAP_KEY || "";
   return (
     <>
       <Iframe
         title="GoogleMap embed"
-        url={getUrl("AIzaSyBZfTnw26CHGdLOcTDZUNgDCA3_DHaYWkU", placeName)}
+        url={getUrl(gmapApiKey || "", placeName)}
         width="100%"
         height="100%"
         loading="lazy"
