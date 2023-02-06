61cd9da2bf562b9c8081349675dd7adbbeb12af8
NIRANKEN <nirank.8192cpjpyjs@gmail.com>
2023-02-06 17:16:43 +0900

    fix envvar

--- app/src/Components/Note.tsx
+++ app/src/Components/Note.tsx
@@ -1,5 +1,8 @@
 import Card from "@mui/material/Card";
+import Grid from "@mui/material/Grid";
+import TextField from "@mui/material/TextField";
 import Typography from "@mui/material/Typography";
+import { useState } from "react";
 
 type NoteProps = {
   travelName: string;
@@ -8,18 +11,43 @@ type NoteProps = {
 
 export const Note: React.FC<NoteProps> = ({ travelName, placeName }) => {
   // travelName, placeNameという型を作っておく, チェック関数の外だし
+  const [noteText, setNoteText] = useState<string>("");
   const checkNames = (travelName: string, placeName: string): boolean =>
     !!travelName && !!placeName;
 
   return checkNames(travelName, placeName) ? (
     <Card>
-      <Typography>{`旅の名前: ${travelName}`}</Typography>
-      <Typography>{`場所: ${placeName}`}</Typography>
-      <Typography>その旅行で行った順番</Typography>
-      <Typography>日記内容</Typography>
-      <Typography>添付写真</Typography>
+      <Grid
+        container
+        spacing={2}
+        direction="column"
+        justifyContent="center"
+        p={2}
+      >
+        <Grid item>
+          <Typography>{`${travelName}／${placeName} の日記`}</Typography>
+        </Grid>
+        <Grid item>
+          <Grid container spacing={2}>
+            <Grid item xs={6}>
+              {/* ダブルクリックしたらフォーカスあたって入力欄になるようにしたい */}
+              <TextField
+                id="travel-note"
+                label="ここに日記を入力してね"
+                sx={{ width: "100%" }}
+                multiline
+                value={noteText}
+                onChange={(event) => setNoteText(event.target.value)}
+              />
+            </Grid>
+            <Grid item xs={6}>
+              <Typography>添付写真</Typography>
+            </Grid>
+          </Grid>
+        </Grid>
+      </Grid>
     </Card>
   ) : (
-    <Typography>日記がないよ</Typography>
+    <Typography>日記がないよ(エラー)</Typography>
   );
 };
