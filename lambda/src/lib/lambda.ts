3774988b0c4736248fe439b9cfec176c4dae5840
NIRANKEN <nirank.8192cpjpyjs@gmail.com>
2023-03-03 00:10:26 +0900

    remove json-body-parser

--- lambda/src/lib/lambda.ts
+++ lambda/src/lib/lambda.ts
@@ -1,8 +1,7 @@
 import middy from "@middy/core"
-import middyJsonBodyParser from "@middy/http-json-body-parser"
 import { Handler } from "aws-lambda"
 
 // TODO: あとでanyを変更する
 export const middyfy = (handler: Handler) => {
-  return middy(handler).use(middyJsonBodyParser())
+  return middy(handler);
 }
